0a5c9b97162175564b9549918ace1a724b0e46f7
Alejandro Londonogit config --global user.email alondono11@gmail.comcdgit config --;istgit config --global user.name Alejandro <alondono11@gmail.com>
2015-05-25 20:37:59 -0400

    removed nav-bar, changed fonts on the header. added faded image to header

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,21 @@
+$(document).ready(function(){
+	// $(window).scroll(function(){
+		var lastScrollTop = 0;
+	$(window).on('scroll', function(){
+		var st = $(this).scrollTop();
+		if(st>lastScrollTop){
+			$(".main-logo").animate({
+				"top": "-=10px",
+				"opacity": "-=0.3"
+			}, {queue: false});
+		}else{
+			$(".main-logo").animate({
+				"top": "+=10px",
+				"opacity": "+=0.3"
+			}, {queue: false});
+		}
+		lastScrollTop = st;
+	});
 
+
+});
