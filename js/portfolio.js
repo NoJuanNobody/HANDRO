339ade536fb8c9b0deb6248055e16f450ac5850f
Alejandro Londonogit config --global user.email alondono11@gmail.comcdgit config --;istgit config --global user.name Alejandro <alondono11@gmail.com>
2015-05-27 18:56:32 -0400

    adding paralax scrolling features first attempt

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,21 +1,25 @@
-$(document).ready(function(){
-	// $(window).scroll(function(){
-		var lastScrollTop = 0;
-	$(window).on('scroll', function(){
-		var st = $(this).scrollTop();
-		if(st>lastScrollTop){
-			$(".main-logo").animate({
-				"top": "-=10px",
-				"opacity": "-=0.3"
-			}, {queue: false});
-		}else{
-			$(".main-logo").animate({
-				"top": "+=10px",
-				"opacity": "+=0.3"
-			}, {queue: false});
-		}
-		lastScrollTop = st;
-	});
+// $(document).ready(function(){
+// 	// $(window).scroll(function(){
+// 		var lastScrollTop = 0;
+// 	$(window).on('scroll', function(){
+// 		var st = $(this).scrollTop();
+// 		if(st>lastScrollTop){
+// 			$(".main-logo").css({
+// 				"top": "-=1px",
+// 				"opacity": "-=0.3"
+// 			}, {queue: false});
+// 		}else{
+// 			$(".main-logo").css({
+// 				"top": "+=1px",
+// 				"opacity": "+=0.3"
+// 			}, {queue: false});
+// 		}
+// 		lastScrollTop = st;
+// 	});
 
 
-});
+// });
+// $(function(){
+// 	var scrollMagicController = new ScrollMagic();
+	
+// });
